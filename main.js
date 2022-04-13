9d01cb820bb48ed65d0a51610475b5f17458b089
Vadim <102861184+RESPAYZ@users.noreply.github.com>
2022-04-13 17:05:52 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,18 @@
+$(function() {
+$('.main-content__slider').slick({
+slidesToShow: 2,
+slidesToScroll: 2,
+arrows: false,
+responsive: [
+
+    {
+      breakpoint: 480,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        dots: true,
+      }
+    }
+  ]
+});
+})
